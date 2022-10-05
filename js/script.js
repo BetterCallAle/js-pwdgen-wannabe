6690915356cf8a718c9f2993582ef8d7b688bda0
BetterCallAle <dap22091998@gmail.com>
2022-10-05 15:49:54 +0200

    input completed

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21 */
 
+//input
+const name = prompt("Inserisci il tuo nome")
+console.log("name");
+
+const surname = prompt("Inserisci il tuo cognome")
+console.log("surname");
+
+const color = prompt("Inserisci il tuo colore preferito")
+console.log("color")
